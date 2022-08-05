cef38ba32cb5005e9e8cf3ca59517bf921d310ba
N-D-P <anshp9918@gmail.com>
2022-08-05 23:41:13 +0530

    Day 2

---
+++ 1. Arrays-DSA/30-sort-0-1-2.cpp
@@ -0,0 +1,17 @@
+Question Link -> https://leetcode.com/problems/sort-colors/
+Video Link -> https://www.youtube.com/watch?v=oaVa-9wmpns
+
+1-> Sort the array
+
+2-> Counting sort 
+    - Map the number of 0s 1s and 2s 
+    - using loop fill the array with 0 1 2
+
+3-> Dutch National Flag Algorithm 
+    - Take three pointers, start , end , index
+    - start will point zeroes, end will point twos, index for traversing.
+    - traverse till index crosses high
+    - when we encounter 0  in index swap nums[start] and nums[index] and increment start and index.
+    - When we encounter 2 swap nums[end], nums[index], just decrement end 
+      *dont Increment index, as we dont know what is at nums[end], it could be zero,one or two*
+
