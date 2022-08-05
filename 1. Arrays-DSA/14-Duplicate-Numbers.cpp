a492b2a5355d1d306326b2879911cc96775372bb
N-D-P <anshp9918@gmail.com>
2022-08-05 23:41:30 +0530

    Day 2

--- 1. Arrays-DSA/14-Duplicate-Numbers.cpp
+++ 1. Arrays-DSA/14-Duplicate-Numbers.cpp
@@ -1,7 +1,13 @@
 video ref-> https://www.youtube.com/watch?v=9YTjXqqJEFE 
 Question Link-> https://leetcode.com/problems/find-the-duplicate-number/
 
-floyd cycle detection method
+1. Naive approach 
+    - Take a map<int,bool>
+    - Loop through array, mark map[nums[i]] = !map[nums[i]]
+    - if(map[nums[i]] is equal to FALSE)
+        that means it is a number that has been encountered previously.
+    - return that number.
+2. floyd cycle detection method
 
     int findDuplicate(vector<int>& nums) {
         //Floyd's Tortoise and Hare (Cycle Detection)
