84ba15a3092397281e45db2ca6cf37bd5bfe2fb1
N-D-P <anshp9918@gmail.com>
2022-08-05 23:48:12 +0530

    Day 2

--- 1. Arrays-DSA/30-sort-0-1-2.cpp
+++ 1. Arrays-DSA/30-sort-0-1-2.cpp
@@ -8,10 +8,38 @@
     - using loop fill the array with 0 1 2
 
 3-> Dutch National Flag Algorithm 
+    - Intuition is all the elements from start to index-1 are 0
+      All the elements from index+1 to end is 2 
     - Take three pointers, start , end , index
     - start will point zeroes, end will point twos, index for traversing.
     - traverse till index crosses high
     - when we encounter 0  in index swap nums[start] and nums[index] and increment start and index.
     - When we encounter 2 swap nums[end], nums[index], just decrement end 
       *dont Increment index, as we dont know what is at nums[end], it could be zero,one or two*
+
+void sortColors(vector<int>& nums) {
+        if(nums.size()==0 || nums.size()==1) return;
+        int start=0,end=nums.size()-1,index=0;
+        
+        while(index<=end)
+        {
+            if(nums[index] == 0)
+            {
+                swap(nums[start],nums[index]);
+                index++;
+                start++;
+            }
+            else if(nums[index] == 2)
+            {
+                swap(nums[index],nums[end]);
+                end--;
+            }
+            else
+            {
+                index++;
+            }
+        }
+        
+    }
+
 
